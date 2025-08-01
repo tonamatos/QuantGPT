8b127cacfb44605eaa90b890a883dbc811c94cbd
Tona <tonamatos@gmail.com>
2025-08-01 15:14:13 -0400

    create pdf_parser

--- src/quantgpt/logging.py
+++ src/quantgpt/logging.py
@@ -1,7 +1,6 @@
 # src/quantgpt/logging.py
 import logging
 
-
 def setup_logging(level: str = "INFO") -> None:
     logging.basicConfig(
         level=getattr(logging, level.upper(), logging.INFO),
